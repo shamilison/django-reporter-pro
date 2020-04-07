8627f681837576af96ca07f54b0238514b1a69ef
shamilison <55080595+shamilison@users.noreply.github.com>
2020-04-07 21:20:41 +0600

    #4 Filter configuration build up done

--- django_report_vue/src/application/views/report/filters/FilterSetting.vue
+++ django_report_vue/src/application/views/report/filters/FilterSetting.vue
@@ -1,66 +1,107 @@
 <template>
     <form class="pa-5 pt-5 pb-5 white">
         <v-text-field label="Field Label" v-model="label"></v-text-field>
-        <v-select :items="filterOptions" label="Condition" v-model="filter"></v-select>
-        <v-btn @click="submit" class="mr-4 primary">ok</v-btn>
-        <v-btn @click="cancel">cancel</v-btn>
+        <v-select :items="filterOptions" dense label="Condition" return-object v-model="filter"></v-select>
+        <v-content :key="filterInputKey" class="mb-4 text-center" v-if="filterInputs.length > 0 || showAddInput">
+            <v-text-field :label="`Input ` + (index + 1)" @click:clear="clearInput(index)" clearable
+                          v-for="(input, index) in filterInputs" v-model="filterInputs[index]"></v-text-field>
+            <v-btn @click="addInput" icon outlined small v-show="showAddInput">
+                <v-icon small>mdi-plus</v-icon>
+            </v-btn>
+        </v-content>
+        <v-btn @click="submit" class="mr-4 primary" small>ok</v-btn>
+        <v-btn @click="cancel" small>cancel</v-btn>
     </form>
 </template>
 
 <script>
+    import ModelInfoMixin from "@/application/views/report/enums/ModelInfoMixin";
+
     export default {
         name: 'FilterSetting',
         components: {},
         props: {
+            field: {
+                type: Object,
+                default: function () {
+                }
+            },
             closeMenu: {
                 type: Function,
                 default: function () {
                 }
             },
         },
-        mixins: [],
+        mixins: [ModelInfoMixin],
         data() {
             return {
                 label: "",
                 filter: null,
                 filterOptions: [],
                 dimensionFilterOptions: [
-                    {value: 'eq', text: 'equals'},
-                    {value: 'neq', text: 'does not equal'},
-                    {value: 'in', text: 'is one of'},
-                    {value: 'not_in', text: 'is not one of'},
-                    {value: 'like', text: 'like'},
-                    {value: 'not_like', text: 'is not like'},
-                    {value: 'regex', text: 'matches regex'},
-                    {value: 'ilike', text: 'like (case insensitive)'},
-                    {value: 'not_ilike', text: 'not like (case insensitive)'},
-                    {value: 'is_null', text: 'is null'},
-                    {value: 'not_null', text: 'is not null'},
+                    {value: 'eq', text: 'equals', input: 1,},
+                    {value: 'neq', text: 'does not equal', input: 1,},
+                    {value: 'in', text: 'is one of', input: -1,},
+                    {value: 'not_in', text: 'is not one of', input: -1,},
+                    {value: 'like', text: 'like', input: 1,},
+                    {value: 'not_like', text: 'is not like', input: 1,},
+                    {value: 'regex', text: 'matches regex', input: 1,},
+                    {value: 'ilike', text: 'like (case insensitive)', input: 1,},
+                    {value: 'not_ilike', text: 'not like (case insensitive)', input: 1,},
+                    {value: 'is_null', text: 'is null', input: 1,},
+                    {value: 'not_null', text: 'is not null', input: 1,},
                 ],
                 measureFilterOptions: [
-                    {value: 'eq', text: 'equals'},
-                    {value: 'neq', text: 'does not equal'},
-                    {value: 'in', text: 'is one of'},
-                    {value: 'not_in', text: 'is not one of'},
-                    {value: 'gt', text: 'greater than'},
-                    {value: 'gte', text: 'greater than or equal'},
-                    {value: 'lt', text: 'less than'},
-                    {value: 'lte', text: 'less than or equal'},
-                    {value: 'numbti', text: 'between and including'},
-                    {value: 'is_null', text: 'is null'},
-                    {value: 'not_null', text: 'is not null'},
+                    {value: 'eq', text: 'equals', input: 1,},
+                    {value: 'neq', text: 'does not equal', input: 1,},
+                    {value: 'in', text: 'is one of', input: -1,},
+                    {value: 'not_in', text: 'is not one of', input: -1,},
+                    {value: 'gt', text: 'greater than', input: 1,},
+                    {value: 'gte', text: 'greater than or equal', input: 1,},
+                    {value: 'lt', text: 'less than', input: 1,},
+                    {value: 'lte', text: 'less than or equal', input: 1,},
+                    {value: 'numbti', text: 'between and including', input: 2,},
+                    {value: 'is_null', text: 'is null', input: 1,},
+                    {value: 'not_null', text: 'is not null', input: 1,},
                 ],
-                sort: null,
-                sortOptions: [
-                    {value: 'ascending', text: 'Ascending'},
-                    {value: 'descending', text: 'Descending'},
-                ],
+                filterInputs: [],
+                filterInputKey: this.$uuid.v4(),
+                showAddInput: false,
             }
         },
         computed: {},
-        watch: {}, created() {
+        watch: {
+            label: function (newVal, oldVal) {
+                this.field['_filter_config']['label'] = newVal;
+            },
+            filter: function (newVal, oldVal) {
+                let result = []
+                if (newVal.input >= 0) {
+                    for (let index = 0; index < newVal.input; index++) {
+                        result.push("");
+                    }
+                    this.showAddInput = false;
+                } else {
+                    this.showAddInput = true;
+                }
+                this.field['_filter_config']['filter'] = newVal.value;
+                this.filterInputs = result;
+            },
+            filterInputs: function (newVal, oldVal) {
+                this.field['_filter_config']['filterInputs'] = newVal;
+            },
+        },
+        created() {
         },
         methods: {
+            addInput: function () {
+                this.filterInputs.push("");
+                this.filterInputKey = this.$uuid.v4();
+            },
+            clearInput: function (index) {
+                this.filterInputs.splice(index, 1);
+                this.filterInputKey = this.$uuid.v4();
+            },
             submit: function () {
                 this.closeMenu();
             },
@@ -69,7 +110,19 @@
             },
         },
         mounted() {
-            this.filterOptions = this.measureFilterOptions;
+            if (this.field['_filter_config'] === undefined) {
+                this.field['_filter_config'] = {};
+                this.field['_filter_config']['label'] = "";
+                this.field['_filter_config']['sort'] = null;
+                this.field['_filter_config']['filter'] = null;
+                this.field['_filter_config']['filterInputs'] = [];
+            }
+            let fieldType = this.getFieldCategory(this.field);
+            if (fieldType === 'measure') {
+                this.filterOptions = this.measureFilterOptions;
+            } else if (fieldType === 'dimension') {
+                this.filterOptions = this.dimensionFilterOptions;
+            }
         }
     };
 </script>
